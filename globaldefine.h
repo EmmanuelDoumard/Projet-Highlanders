0845b5ed5efae573326fc7a742ebc4f14a5af6c9
Korbenik <emmanuel.doumard@gmail.com>
2016-05-03 16:32:16 +0200

    Update globaldefine.h

--- globaldefine.h
+++ globaldefine.h
@@ -1,5 +1,5 @@
-// fichier contenant l'inclusion des typedef et #define qui servent à toute l'application
-// à inclure dans tous les fichiers
+// fichier contenant l'inclusion des typedef et #define qui servent Ã  toute l'application
+// Ã  inclure dans tous les fichiers
 
 #include "lpc17xx.h"
 #include "lpc17xx_gpio.h"
@@ -8,9 +8,13 @@
 #include "lpc17xx_timer.h"
 #include "lpc17xx_uart.h"
 
-// Déclaration des constantes
+#include "ili_lcd_general.h"
+#include "lcd_api.h"
+#include "bula.h"
+
+// DÃ©claration des constantes
 
 #define TIMER0_TIME_STEP			1		// 1 us de Step Timer (25 000/25000)
 #define TIMER0_MATCH_VALUE_100us  100 // Match value pour avoir 100 us
 #define MATCH_VALUE1 1000 // A utiliser dans l'interruption, correspond a 1
-#define DUREE_BIP 1000      // Durée d'un bip (à multiplier par 100us) => 0.1s
+#define DUREE_BIP 1000      // DurÃ©e d'un bip (Ã  multiplier par 100us) => 0.1s

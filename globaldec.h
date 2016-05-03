22b4d515de360e3a33f095aa3fe3897f070ff407
Bencaddyro <benthedah@hotmail.fr>
2016-05-03 16:07:12 +0200

    Update globaldec.h

--- globaldec.h
+++ globaldec.h
@@ -1,9 +1,19 @@
 #include "LPC17xx.h"
 
-// fichier contenant la d�claration des variables globales de l'application
-// � inclure seulement dans le main
+// fichier contenant la déclaration des variables globales de l'application
+// à inclure seulement dans le main
 
 int toto;
 
 char bufferTX[16]; // buffer pour l'affichage sur uart0
-int n;  // taille du message � envoyer (max 16)
+int n;  // taille du message à envoyer (max 16)
+extern int n;
+extern int TIMER0_VAR100US; // Base de temps de 100 us pour timer0, se reset pour des raisons pratique
+extern int TIMER0_VAR100USROLAND; // Spécial Roland
+extern int TIMER0_TEMPS; // Définit le temps "absolu" depuis le début du timer0 (*100us)
+extern int BIP;                // "Booléen" qui indique si le bip est actif
+extern int NB_BIP;
+extern int MATCH_BIP;
+extern int MATCH_VALUE_FQBIP;
+extern int echo;
+extern int trigger;
